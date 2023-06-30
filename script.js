28b4de2b03f2b545e7e1a3dde5832dcee72c5cc1
Dremeret <terletskyyd2010@gmail.com>
2023-06-30 17:45:21 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+let age = 13;
+console.log(age);
+
+
+
+var name = 'Dimooon';
+console.log(name);
+
+
+var isStudent = false;
+console.log(isStudent);
+
+
+var myString =
+  'Найкраща помста — величезний успіх. Френк Сінатра';
+console.log(myString);
+
+
+let myNumber = 756;
+myNumber += 10;
+
+console.log(myNumber);
+
+
+
+var myNull = null;
+console.log(myNull);
+
+
+
+var yourName = prompt('Введіть своє імя:');
+
+alert('Привіт, ' + yourName + '!');
+
+
+
+var confirm = confirm('Підтвердіть:');
+
+if (confirm == true) {
+  alert('Дякую за підтвердження!');
+} else {
+  alert('Дію відмінено!');
+}
+
+ 
+
+alert('Дія небезпечна!');
+var confirm = confirm('Підтвердіть:');
+
+if (confirm == true) {
+  alert('Дякую за підтвердження!');
+} else {
+  alert('Дію відмінено!');
+}
