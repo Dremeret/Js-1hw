b01cdf46ec33e5a1a3aee7495be1a1ab3e4bc814
Dremeret <terletskyyd2010@gmail.com>
2023-07-13 16:36:57 +0300

    First commit

--- script.js
+++ script.js
@@ -1,56 +1,101 @@
 
 
-let age = 13;
-console.log(age);
+// let age = 13;
+// console.log(age);
 
 
 
-var name = 'Dimooon';
-console.log(name);
+// var name = 'Dimooon';
+// console.log(name);
 
 
-var isStudent = false;
-console.log(isStudent);
+// var isStudent = false;
+// console.log(isStudent);
 
 
-var myString =
-  'Найкраща помста — величезний успіх. Френк Сінатра';
-console.log(myString);
+// var myString =
+//   'Найкраща помста — величезний успіх. Френк Сінатра';
+// console.log(myString);
 
 
-let myNumber = 756;
-myNumber += 10;
+// let myNumber = 767;
+// myNumber += 10;
 
-console.log(myNumber);
-
-
-
-var myNull = null;
-console.log(myNull);
+// console.log(myNumber);
 
 
 
-var yourName = prompt('Введіть своє імя:');
-
-alert('Привіт, ' + yourName + '!');
-
+// var myNull = null;
+// console.log(myNull);
 
 
-var confirm = confirm('Підтвердіть:');
 
-if (confirm == true) {
-  alert('Дякую за підтвердження!');
-} else {
-  alert('Дію відмінено!');
-}
+// var yourName = prompt('Введіть своє імя:');
+
+// alert('Привіт, ' + yourName + '!');
+
+
+
+// var confirm = confirm('Підтвердіть:');
+
+// if (confirm == true) {
+//   alert('Дякую за підтвердження!');
+// } else {
+//   alert('Дію відмінено!');
+// }
 
  
 
-alert('Дія небезпечна!');
-var confirm = confirm('Підтвердіть:');
+// alert('Дія небезпечна!');
+// var confirm = confirm('Підтвердіть:');
 
-if (confirm == true) {
-  alert('Дякую за підтвердження!');
-} else {
-  alert('Дію відмінено!');
-}
+// if (confirm == true) {
+//   alert('Дякую за підтвердження!');
+// } else {
+//   alert('Дію відмінено!');
+// }
+
+
+//  Home work2
+
+// let celsius = 10;
+// let farenheit = celsius * 1.8 + 32 ;
+// console.log(farenheit);
+
+
+// let monthDays = 31;
+// let  hoursMonth = monthDays *24 ;
+// let minuteMonth = hoursMonth*60 ;
+// console.log( hoursMonth);
+// console.log(minuteMonth);
+
+
+// let healt =100 ;
+// let energy = 100;
+
+// console.log(healt-23);
+// console.log(energy-38);
+
+
+// let sum = 1000;
+// let discount = sum *10 /100 ;
+// console.log(sum);
+
+
+// let number = 31.916;
+// console.log(Math.floor(number));
+
+
+// let num = 3.8;
+// console.log(parseFloat(num));
+
+
+let string = "15";
+console.log(parseInt(string));
+
+
+// let numberTwo = 46 ;
+// console.log(Math.sqrt(numberTwo));
+
+
+// let numTwo = 678;
