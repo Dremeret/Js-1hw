5bd71a95f5014749c723af548b975cca85cf80d8
Dremeret <terletskyyd2010@gmail.com>
2023-07-24 15:55:31 +0300

    First commit

--- script.js
+++ script.js
@@ -58,42 +58,62 @@
 
 //  Home work2
 
-let celsius = 10;
-let farenheit = celsius * 1.8 + 32 ;
-console.log(farenheit);
+// let celsius = 10;
+// let farenheit = celsius * 1.8 + 32 ;
+// console.log(farenheit);
 
 
-let monthDays = 31;
-let  hoursMonth = monthDays *24 ;
-let minuteMonth = hoursMonth*60 ;
-console.log( hoursMonth);
-console.log(minuteMonth);
+// let monthDays = 31;
+// let  hoursMonth = monthDays *24 ;
+// let minuteMonth = hoursMonth*60 ;
+// console.log( hoursMonth);
+// console.log(minuteMonth);
 
 
-let healt =100 ;
-let energy = 100;
+// let healt =100 ;
+// let energy = 100;
 
-console.log(healt-23);
-console.log(energy-38);
-
-
-let sum = 1000;
-let discount = sum *10 /100 ;
-console.log(sum);
+// console.log(healt-23);
+// console.log(energy-38);
 
 
-let number = 31.916;
-console.log(Math.floor(number));
+// let sum = 1000;
+// let discount = sum *10 /100 ;
+// console.log(sum);
 
 
-let num = 3.8;
-console.log(parseFloat(num));
+// let number = 31.916;
+// console.log(Math.floor(number));
 
 
-let string = "15";
-console.log(parseInt(string));
+// let num = "3.8";
+// console.log(parseFloat(num));
 
 
-let numberTwo = 46 ;
-console.log(Math.sqrt(numberTwo));
+// let string = "15";
+// console.log(parseInt(string));
 
+
+// let numberTwo = 46 ;
+// console.log(Math.sqrt(numberTwo));
+
+
+// HOME work 3
+
+
+
+let result = 5 + 5 + "5";
+console.log(result);
+console.log (typeof result);
+
+
+let email =  "dream.er03062006@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+let fullName = ` ${word1}  ${word2}   ${word3} Viktor `;
+console.log(fullName);
